b0ce64a366634d97050083ef75fcf277e911386a
TheUnforg1ven <tbatjuk4u@gmail.com>
2024-10-15 22:19:18 +0300

    Fixed angular initial files and git

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +7,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'Test';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (responce) => (this.users = responce),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
 }
